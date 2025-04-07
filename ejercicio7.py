a194724827cb249533fdafd4aa10e823e0d6ea97
mariadelaorden <mariadelaorden2005@hotmail.com>
2025-04-07 20:38:25 +0200

    tests 7

--- ejercicio7.py
+++ ejercicio7.py
@@ -38,3 +38,48 @@ b = [1,0,1,0,0,1,0,0,1]
 long, subsecuencia = secuencias(a,b)
 print("Longitud maxima: ", long)
 print("Subsecuencia común", subsecuencia)
+
+def test_secuencias():
+    #Prueba 1: secuencias cortas 
+    a1 = [1,0,1,1]
+    b1 = [0,1,1,0]
+
+    long_esp1 = 3
+    sec_esp1 = [0,1,1]
+
+    long1, subsec1 = secuencias(a1,b1)
+    assert long_esp1 == long1
+    assert sec_esp1 == subsec1
+
+    #Prueba 2: secuencias largas
+    a2 = [0,1,1,0,1,0,1,0]
+    b2 = [1,0,1,0,0,1,0,0,1]
+
+    long_esp2 = 6
+    sec_esp2 = [0,1,1,0,0,1]
+
+    long2, subsec2 = secuencias(a2,b2)
+    assert long_esp2 == long2
+    assert sec_esp2 == subsec2
+    
+    #Prueba 3: no hay subsecuencias en común
+    a3 = [0,0,0]
+    b3 = [1,1,1]
+
+    long_esp3 = 0
+    sec_esp3 = []
+
+    long3, subsec3 = secuencias(a3,b3)
+    assert long_esp3 == long3
+    assert sec_esp3 == subsec3
+
+    #Prueba 4: coinciden 
+    a4 = [1,0,1,1]
+    b4 = [1,0,1,1]
+
+    long_esp4 = 4
+    sec_esp4 = [1,0,1,1]
+
+    long4, subsec4 = secuencias(a4,b4)
+    assert long_esp4 == long4
+    assert sec_esp4 == subsec4
